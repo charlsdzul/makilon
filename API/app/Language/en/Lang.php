96bdccb77d5ce19565eabd60f64fcd5d65d0b484
Charls Dzul <61024153+charlsdzul@users.noreply.github.com>
2023-09-19 20:38:23 +0000

    Update Lang.php and util..js

--- API/app/Language/en/Lang.php
+++ API/app/Language/en/Lang.php
@@ -1,235 +1,98 @@
 <?php
 
 return [
-    //100
-    "tipoDatoIncorreto" => "Tipo de dato incorrecto",//100
-	"longitudDatoIncorreto" => "Longitud de dato incorrecto",//101
-	"datoInvalido" => "Dato inválido",//102
-	"datoContieneEspaciosOCaracteresInv" => "Dato contiene espacios en blanco o caracteres inválidos",//103
-
-	//200
-	"datosNoRecibidos" => "No recibimos tus datos",//200
-	"datosActualizados" => "Tus datos se actualizaron con éxito",//201
-	"datoNoActualizados" => "No pudimos actualizar tus datos",//202
-	"errorDatosValidacion" => "Datos no pasaron validación", //203
-
-    //800
-	"solicitudNoProcesada" => "Su solicitud no se pudo procesar.",//801
-	"queryParamsInvalidos" => "Parámetro {0} de URL inválido.",//803
-	"idInvalido" => "ID inválido.",//802
-	"intentoNoLogueado" => "Intento de accion sin estar logueado",//800
-	"errorDatos" => "Error en los datos recibidos.",
-
-    //1000
-    "correo" => "Correo invalido",
-    "contrasena" => "Contraseña invalida",//1002
-
-    "errorCorreo" => "Correo invalido",
-
-    "errorCorreo" => "Correo invalido",
-	"ingresaCorreo" => "Ingresa un correo",
-    "ingresaCorreoValido" => "Ingresa un correo valido",
-    "errorContrasena" => "Contraseña invalida",//1002
-
-    "correoInvalido" => "Dato no tiene formato de correo",//1001
-	"contrasenaEnBlanco" => "Ingresa una contraseña",//1002
-	"contrasenaMinima" => "Contraseña debe tener mínimo 8 caracteres",//1003
-	"contrasenaMaxima" => "Contraseña debe tener máximo 15 caracteres",//1004
-	"contrasenaInvalida" => "Contraseña inválida",//1005
-	"contrasenasNoIguales" => "Contraseñas ingresadas deben ser iguales",//1006,
-
-    
-
-    "labels" => [
-        "correo" => "Correo",
-            "contrasena" => "Contraseña",
-
-
-//"contrasenaMinima" => "Contraseña debe tener mínimo 8 caracteres",
-//"contrasenaMaxima" => "Contraseña debe tener máximo 15 caracteres",
-//"contrasenaInvalida" => "Contraseña inválida",
-//"contrasenasNoIguales" => "Contraseñas ingresadas deben ser iguales",	
-//"sesionIniciada" => "Sesion iniciada",
-],
-
-
-      "login" => [
-        			"errorLogin" => "Error en inicio de sesion",
-                    	"correoContrasenaInvalida" => "Correo y/o contraseña inválidos",
-	     	"cuentaInactiva" => "Lo sentimos, tu cuenta se encuentra inactiva",
-
-
-		//"contrasenaMinima" => "Contraseña debe tener mínimo 8 caracteres",
-			//"contrasenaMaxima" => "Contraseña debe tener máximo 15 caracteres",
-			//"contrasenaInvalida" => "Contraseña inválida",
-			//"contrasenasNoIguales" => "Contraseñas ingresadas deben ser iguales",	
-			//"sesionIniciada" => "Sesion iniciada",
+	"title" => [
+		"errorLogin" => "Error en inicio de sesión",
+		 "reestablecerMiContra" => "Reestablecer mi contraseña",
 	],
 
-];
-	
-	
+	"label" => [
+		"correo" => "Correo",
+		"contrasena" => "Contraseña",
+		"usuario" => "Usuario",
+		"token" => "Token",
 
-	//------
-	//"correo" => "Correo",
-	//"contrasena" => "Contraseña",
+	],
 
+	"detail" => [
+		//Common		
+		"intentoNoLogueado" => "Intento de acción sin estar logueado",
+		"idInvalido" => "ID inválido.",
+		"errorProcesarSolicitud" => "No se pudo procesar tu solicitud, intenta más tarde.",
+
+		//datos
+		"errorDatosRecibidos" => "Error en los datos recibidos.",
+		"datoInvalido" => "Dato inválido",
+		"datoContieneEspaciosOCaracteresInv" => "Dato contiene espacios en blanco o caracteres inválidos",
+		"errorDatosValidacion" => "Datos no pasaron validación", 
+		"tipoDatoIncorreto" => "Tipo de dato incorrecto",
+		"longitudDatoIncorreto" => "Longitud de dato incorrecto",
+		"datosNoRecibidos" => "No recibimos tus datos",
+		"datosActualizados" => "Tus datos se actualizaron con éxito",
+		"datoNoActualizados" => "No pudimos actualizar tus datos",
+		"datosNoActualizados" => "No se pudieron actualizar tus datos",
+
+		//cuenta
+		"cuentaInactiva" => "Lo sentimos, tu cuenta se encuentra inactiva",
+		"errorCrearCuenta" => "Por el momento no podemos crear su cuenta. Intenta más tarde.",
+		"correoCodigoNoExisten" => "Hmm, no pudimos confirmar tu cuenta. Por favor, mandanos un aviso.",
+		"correoYaEstaRegistrado" => "Hey, tu correo ya está registrado. Ya puedes iniciar sesión.",
+		"correoCodigoInvalido" => "No pudimos confirmar tu cuenta. Intentalo más tarde.",
+		"registroExitoso" => "Hey, tu cuenta ha sido confirmada. Ya puedes iniciar sesion..",
+
+		//sesión	
+		"sesionIniciada" => "Sesión iniciada",
+		"errorInicioSesion" => "Se detecto un problema al iniciar sesión.",
+	    "errorCerrarSesion" => "Se detecto un problema al cerrar sesión.",
+	    "errorFinalizarSesion" => "Se detecto un problema al finalizar sesión.",
+	    "sesionFinalizada" => "Sesión finalizada",
+		"preregistroExitoso" => "Ya solo falta un paso más. Te hemos enviado un mensaje de confirmacion a tu correo. ",
+
+		//correo
+		"correoContrasenaInvalida" => "Correo y/o contraseña inválidos",	
+		"correoNoDisponible" => "El correo ingresado no está disponible para usarse",
+		"ingresaCorreo" => "Ingresa un correo",
+		"ingresaCorreoValido" => "Ingresa un correo válido",
+		"correoInvalido" => "Correo inválido",
+		"correoActualizado" => "Tu correo ha sido actualizado",
+		"datosActualizados" => "Tus datos han sido actualizados",
+
+		//contraseña
+		"contrasenaMinima" => "Contraseña debe tener mínimo 8 caracteres",
+		"contrasenaMaxima" => "Contraseña debe tener máximo 15 caracteres",
+		"contrasenasNoIguales" => "Contraseñas ingresadas deben ser iguales",
+		"contrasenaInvalida" => "Contraseña inválida",  
+		"contrasenaEnBlanco" => "Ingresa una contraseña",
+	    "contrasenaNoActualizada" => "No se pudo actualizar tu contraseña",
+		"contrasenaActualizada" => "Tu contraseña ha sido actualizada",
+		"respuestaRecuperarPassword" => "Si el correo que ingresaste es válido, te enviaremos las indicaciones para reestablecer tu contraseña.",
+
+		//API	
+		"solicitudNoProcesada" => "Su solicitud no se pudo procesar.",
+		"queryParamsInvalidos" => "Parámetro {0} de URL inválido.",
+        "accesoNoPermitido" => "Acceso no permitido",
+
+		//token
+		"tokenInvalido" => "Token Inválido",
+
+		//email
+		"emailAsunto" => "Registro Pulpox",
+		"emailCuerpo" => "Para confirmar tu correo (<b>{0}</b>) y registro, da click en este enlace -> {1}",
+	    "emailFrom" => "Pulpox",
+		"emailAsuntoRecContra" => "Recuperación de contraseña",
+        "emailCuaerpoRecContra" => "Para recuperar tu contraseña, da click en este enlace -> {1}",
 		
-	//1000
-   
+		//url
+		"urlInicio" => "Inicio",
+        "urlIngresar" => "Ingresar",
+        "urlCrearCuenta" => "Crear cuenta",
+        "urlMisAnuncios" => "Mis Anuncios",
+        "urlNuevoAnuncio" => "Nuevo Anuncio",
+        "urlMiCuenta" => "Mi Cuenta",
+        "urlConfiguraciones" => "Configuración",
+        "urlSalir" => "Salir",	
+	],    
 
-
-    //--
-	//"usuario" => "Usuario",
-	//"correoNoDisponible" => "El correo ingresado no está disponible para usarse",
-	//"errorCrearCuenta" => "Por el momento no podemos crear su cuenta. Intenta más tarde.",
-	//"correoCodigoNoExisten" => "Hmm, no pudimos confirmar tu cuenta. Por favor, mandanos un aviso.",
-	//"correoYaEstaRegistrado" => "Hey, tu correo ya está registrado. Ya puedes iniciar sesión.",
-	//"correoCodigoInvalido" => "No pudimos confirmar tu cuenta. Intentalo más tarde.",
-	//"registroExitoso" => "Hey, tu cuenta ha sido confirmada Ya puedes iniciar sesion..",
-	//"correoContrasenaInvalida" => "Correo y/o contraseña inválidos.",
-	//"sesionIniciada" => "Sesion iniciada",
-	//"errorInicioSesion" => "Se detecto un problema al iniciar sesión.",
-	//"errorCerrarSesion" => "Se detecto un problema al cerrar sesión.",
-	//"errorFinalizarSesion" => "Se detecto un problema al finalizar sesión.",
-	//"sesionFinalizada" => "Sesion finalizada",
-	//"passwordNoActualizado" => "No se pudo actualizar tu contraseña",
-	//"passwordActualizado" => "Tu contraseña ha sido actualizada",
-	//"correoNoActualizado" => "No se pudo actualizar tu correo",
-	//"correoActualizado" => "Tu correo ha sido actualizado",
-	//"datosActualizados" => "Tus datos han sido actualizados",
-	//"datosNoActualizados" => "No se pudieron actualizar tus datos",
-	//"respuestaRecuperarPassword" => "Si el correo que ingresaste es válido, te enviaremos las indicaciones para reestablecer tu contraseña.",
-	//"errorRecuperarPassword" => "No se pudo procesar tu solicitud, intenta más tarde.",
-	
-
-  
-
-   // "registro" => [
-			
-        //"contrasenasNoIguales" => "Contraseñas ingresadas deben ser iguales",	
-        
-   // ],
-
-
-    //"recuperarContrasena" => [
-            
-   // ],
-
-
-   // "email" => [
-		//"emailAsunto" => "Registro Pulpox",
-		//"emailCuerpo" => "Para confirmar tu correo (<b>{0}</b>) y registro, da click en este enlace -> {1}",
-		//"preregistroExitoso" => "Ya solo falta un paso más. Te hemos enviado un mensaje de confirmacion a tu correo. ",
-	
-        //"generalFrom" => "Pulpox",
-
-        //"recuperacionPassword" => [
-            //"reestablecerMiContra" => "Reestablecer mi contraseña",
-            //"asunto" => "Recuperación de contraseña",
-            //"cuerpo" => "Para recuperar tu contraseña, da click en este enlace -> {1}",
-        //],
-   // ],
-
-   // "jwt" => [
-		//"jwtInvalido" => "Token Invalido",
-	//],
-
-  //  "api" => [
-        //"accesoNoPermitido" => "Acceso no permitido",
-	//],
-
-   // "url" => [
-        //"urlInicio" => "Inicio",
-        //"urlIngresar" => "Ingresar",
-        //"urlCrearCuenta" => "Crear cuenta",
-        //"urlMisAnuncios" => "Mis Anuncios",
-        //"urlNuevoAnuncio" => "Nuevo Anuncio",
-        //"urlMiCuenta" => "Mi Cuenta",
-        //"urlConfiguraciones" => "Configuración",
-        //"urlSalir" => "Salir",	
-   // ],
-
-
-        //1000
-	//"correoEnBlanco" => "Ingresa un correo",
-	//"correoInvalido" => "Correo inválido",
-	//"contrasenaEnBlanco" => "Ingresa una contraseña",
-	//"contrasenaMinima" => "Contraseña debe tener mínimo 8 caracteres",
-	//"contrasenaMaxima" => "Contraseña debe tener máximo 15 caracteres",
-	//"contrasenaInvalida" => "Contraseña inválida",
-	//"contrasenasNoIguales" => "Contraseñas ingresadas deben ser iguales",
-
-	//--
-	//"usuario" => "Usuario",
-	//"correoNoDisponible" => "El correo ingresado no está disponible para usarse",
-	//"errorCrearCuenta" => "Por el momento no podemos crear su cuenta. Intenta más tarde.",
-	//"correoCodigoNoExisten" => "Hmm, no pudimos confirmar tu cuenta. Por favor, mandanos un aviso.",
-	//"correoYaEstaRegistrado" => "Hey, tu correo ya está registrado. Ya puedes iniciar sesión.",
-	//"cuentaInactiva" => "Lo sentimos, tu cuenta se encuentra inactiva",
-	//"correoCodigoInvalido" => "No pudimos confirmar tu cuenta. Intentalo más tarde.",
-	//"registroExitoso" => "Hey, tu cuenta ha sido confirmada Ya puedes iniciar sesion..",
-	//"correoContrasenaInvalida" => "Correo y/o contraseña inválidos.",
-	//"sesionIniciada" => "Sesion iniciada",
-	//"errorInicioSesion" => "Se detecto un problema al iniciar sesión.",
-	//"errorCerrarSesion" => "Se detecto un problema al cerrar sesión.",
-	//"errorFinalizarSesion" => "Se detecto un problema al finalizar sesión.",
-	//"sesionFinalizada" => "Sesion finalizada",
-	//"passwordNoActualizado" => "No se pudo actualizar tu contraseña",
-	//"passwordActualizado" => "Tu contraseña ha sido actualizada",
-	//"correoNoActualizado" => "No se pudo actualizar tu correo",
-	//"correoActualizado" => "Tu correo ha sido actualizado",
-	//"datosActualizados" => "Tus datos han sido actualizados",
-	//"datosNoActualizados" => "No se pudieron actualizar tus datos",
-	//"respuestaRecuperarPassword" => "Si el correo que ingresaste es válido, te enviaremos las indicaciones para reestablecer tu contraseña.",
-	//"errorRecuperarPassword" => "No se pudo procesar tu solicitud, intenta más tarde.",
-	//"errorLogin" => "Error en inicio de sesion.",
-
-	//"email" => [
-		//"emailAsunto" => "Registro Pulpox",
-		//"emailCuerpo" => "Para confirmar tu correo (<b>{0}</b>) y registro, da click en este enlace -> {1}",
-		//"preregistroExitoso" => "Ya solo falta un paso más. Te hemos enviado un mensaje de confirmacion a tu correo. ",
-	//],
-	//"jwt" => [
-		//"jwtInvalido" => "Token Invalido",
-	//],
-
-	//"Common" => [
-		//"correoEnBlanco" => "Ingresa un correo",
-		//"correoInvalido" => "Correo inválido",
-		//"contrasenaEnBlanco" => "Ingresa una contraseña",
-
-//	],
-
-
-	//	"Login" => [
-		
-			//"contrasenaMinima" => "Contraseña debe tener mínimo 8 caracteres",
-			//"contrasenaMaxima" => "Contraseña debe tener máximo 15 caracteres",
-			//"contrasenaInvalida" => "Contraseña inválida",
-			//"contrasenasNoIguales" => "Contraseñas ingresadas deben ser iguales",	
-			//"sesionIniciada" => "Sesion iniciada",
-			//"errorLogin" => "Error en inicio de sesion.",	
-	//	],
-	//	"Registro" => [
-			
-			///"contrasenasNoIguales" => "Contraseñas ingresadas deben ser iguales",	
-			
-	//	],
-	//	"RecuperarContrasena" => [
-				
-	//	],
-
-
-
-
-
-
-
-  //  "cccc" => [
-	//	"aaaa" => "bbb",
-	//	"aaaa" => "bbb",
-	//	"aaaa" => "bbb",      
-	//],
-	
-
+	"action" => [
+	],
+] 
--- ClientAPP/makilon/src/Utils/util..js
+++ ClientAPP/makilon/src/Utils/util..js
@@ -30,3 +30,66 @@ export const showModal = ({ type, title, content }) => {
 		Modal.error({ title: title, content: content });
 	}
 };
+
+export const getErrorMessages = ({errors=errors,useLabel=true, useDetail=true, useAction=true,separator="<br>"})=>{
+
+	const generateErrorMensaje = ({error,useLabel, useDetai, useAction}) =>{
+	
+	let mensaje = ""
+	
+	if(useLabel &&  error.label){
+	mensaje = `${error.label}:`
+	}
+	
+	
+	  if(useDetail && error.detail){
+	 if(mensaje){
+	 mensaje =  `${mensaje} ${error.detail}.`
+	 }else{
+	 mensaje = `${error.detail}.`
+	 }
+	
+	}
+	 
+	
+	  if(useAction && error.action){
+	  
+		if(mensaje){
+	 mensaje =  `${mensaje} ${error.action}.`
+	 }else{
+	 mensaje = `${error.action}.`
+	 }
+	 
+	 
+	}
+	
+	  console.log(mensaje)
+  
+	
+	return mensaje
+	
+	}
+  
+	if(errors.length===1){
+	 const error = errors[0]
+  
+	return generateErrorMensaje({error,useLabel,useDetail,useAction})
+  
+	}else if (errors.length>1){
+	let mensajes = ""
+	
+	for (const index in errors) {
+   // console.log(error)
+   const error = errors[index]
+	  const mensaje = generateErrorMensaje({error,useLabel,useDetail,useAction});
+	  mensajes = mensajes + mensaje + separator
+	  // ...
+	  }
+	
+	
+	
+	return mensajes
+  
+  
+	}
+  }
